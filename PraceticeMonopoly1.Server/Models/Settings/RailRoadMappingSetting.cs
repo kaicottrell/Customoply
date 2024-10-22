e92145103b191cc99fbbf58c3d6804dbe1bc8b22
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-10-21 21:15:17 -0600

    Added RailRoad settings for dynamically determining the rent cost associated with a dynamic range of owned railroads

---
+++ PraceticeMonopoly1.Server/Models/Settings/RailRoadMappingSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomMonopoly.Server.Models.Settings
+{
+    /// <summary>
+    /// Settings outlining the number of railroads and the associated rent
+    /// </summary>
+    public class RailRoadMappingSetting
+    {
+        [Key]
+        public int Id {  get; set; }
+        /// <summary>
+        /// The user fk relating to the <see cref="ApplicationUser"/> who owns the setting
+        /// </summary>
+        public int UserId { get; set; }
+        public int RentCost { get; set; }
+        public int NumberOfRailRoadsOwned { get; set; }
+    }
+}
