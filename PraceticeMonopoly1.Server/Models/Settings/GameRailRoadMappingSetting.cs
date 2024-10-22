7b570172a638fae8d5d5c8d1e78b9acfd1cd9412
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-10-22 17:56:08 -0600

    Mapping settings

---
+++ PraceticeMonopoly1.Server/Models/Settings/GameRailRoadMappingSetting.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomMonopoly.Server.Models.Settings
+{
+    //Associative table for mapping games to railroadmapping settings
+    public class GameRailRoadMappingSetting
+    {
+        [Key, Column(Order = 1)]
+        public int GameId { get; set; }
+        [Key, Column(Order = 2)]
+        public int RailRoadMappingSettingId { get; set; }
+        [ForeignKey("RailRoadMappingSettingId")]
+        public RailRoadMappingSetting RailRoadMappingSetting { get; set; }
+        [ForeignKey("GameId")]
+        public Game Game { get; set; }
+    }
+}
