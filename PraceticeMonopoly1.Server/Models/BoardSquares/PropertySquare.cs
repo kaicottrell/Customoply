c89d1cbb18e0647dcaec0e66bea0b5a79eddac51
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-10-21 21:17:02 -0600

    Added additional specifications for the rent per house count, and a  railroadsquare as a type of boardsquare that can be landed on.

--- PraceticeMonopoly1.Server/Models/BoardSquares/PropertySquare.cs
+++ PraceticeMonopoly1.Server/Models/BoardSquares/PropertySquare.cs
@@ -4,10 +4,14 @@
     {
         public string Name { get; set; }
         public string Color { get; set; }
-        public int Rent { get; set; }
+        public int RentNoHouse { get; set; }
+        public int RentOneHouse { get; set; }
+        public int RentTwoHouse { get; set; }
+        public int RentThreeHouse { get; set; }
+        public int RentFourHouse { get; set; }
+        public int RentHotel { get; set; }
         public int MorgageValue { get; set; }
-        public int Cost { get; set; }
-        public bool IsRailRoad { get; set; }
+        public int Price { get; set; }
         public ICollection<PlayerProperty> PlayerProperties { get; set; }
     }
 }
---
+++ PraceticeMonopoly1.Server/Models/BoardSquares/RailRoadSquare.cs
@@ -0,0 +1,14 @@
+﻿using CustomMonopoly.Server.Models.Settings;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomMonopoly.Server.Models.BoardSquares
+{
+    public class RailRoadSquare : PropertySquare
+    {
+        /// <summary>
+        /// Stores the Mappings used for  
+        /// </summary>
+       [NotMapped]
+       public List<RailRoadMappingSetting> RailRoadRentMappings { get; set; }
+    }
+}
